ea7cf6affc7a6acf19db0594d2b4d7267f85c0bf
neelarani <neela9624@gmail.com>
2024-10-06 11:33:41 +0600

    added categories function

--- js/script.js
+++ js/script.js
@@ -3,31 +3,71 @@ const loadCategories = async () => {
     `https://openapi.programming-hero.com/api/peddy/categories`
   );
   const data = await response.json();
-  displayPets(data.categories);
+  displayCategories(data.categories);
 };
 
-const displayPets = pets => {
-  const petContainer = document.getElementById('pet-container');
-  pets.forEach(pet => {
+const displayCategories = categories => {
+  const categoryContainer = document.getElementById('category-container');
+  categories.forEach(category => {
     const div = document.createElement('div');
     div.innerHTML = `
-    <div class="flex justify-center items-center font-extrabold gap-2">
-    <img src="${pet.category_icon}"/>
-    <span>${pet.category}</span>
+    <div onclick="loadAllPet('${category.category}')" class="flex justify-center items-center font-extrabold gap-2 border-2 px-8 py-3 cursor-pointer">
+
+    <img src="${category.category_icon}"/>
+    <span>${category.category}</span>
     </div>
     `;
-    petContainer.appendChild(div);
+    categoryContainer.appendChild(div);
   });
 };
 
 loadCategories();
 
-const loadAllPet = async () => {
+// loading all pets
+const loadAllPet = async (category = '') => {
   const response = await fetch(
     `https://openapi.programming-hero.com/api/peddy/pets`
   );
   const data = await response.json();
-  console.log(data);
+  const filterPets = category
+    ? data.pets.filter(pet => pet.category === category)
+    : data.pets;
+  displayAllPets(filterPets);
+};
+
+const displayAllPets = pets => {
+  const displayPets = document.getElementById('display-pets');
+  displayPets.innerHTML = '';
+  pets.forEach(pet => {
+    const div = document.createElement('div');
+    div.innerHTML = `
+            <div class="card bg-base-100 w-80  shadow-xl h-[400px]  object-cover">
+              <figure class="p-4">
+                <img src=${pet.image} />
+              </figure>
+              <div class="card-body">
+                <h2 class="card-title">${pet.breed}</h2>
+                <p><i class="fa-regular fa-calendar-days p-2"></i>
+                ${pet.date_of_birth}</p>
+                <p><i class="fa-solid fa-mercury"></i>
+                  Gender: ${pet.gender}
+                </p>
+                <p><i class="fa-solid fa-dollar-sign"></i>
+                Price: ${pet.price}
+                </p>
+                <div class="card-actions flex justify-between">
+                  <button class="btn ">
+                    <i class="fa-regular fa-thumbs-up text-lg"></i>
+                  </button>
+                  <button class="btn text-[#0E7A81] text-lg">Adopt</button>
+                  <button class="btn text-[#0E7A81] text-lg">Details</button>
+                </div>
+              </div>
+            </div>
+
+  `;
+    displayPets.appendChild(div);
+  });
 };
 
 loadAllPet();
