2c8f747a316d9df25e781f0e47c902cbed7ba12d
neelarani <neela9624@gmail.com>
2024-10-07 13:49:48 +0600

    modal added

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const loadCategories = async () => {
   displayCategories(data.categories);
 };
 
+// display category function
 const displayCategories = categories => {
   const categoryContainer = document.getElementById('category-container');
   categories.forEach(category => {
@@ -46,6 +47,7 @@ const handleAddPet = pet => {
   selectPet.appendChild(div);
 };
 
+// display all pets
 const displayAllPets = pets => {
   const displayPets = document.getElementById('display-pets');
   displayPets.innerHTML = '';
@@ -61,6 +63,7 @@ const displayAllPets = pets => {
     displayPets.appendChild(div);
     return;
   }
+
   pets.forEach(pet => {
     const div = document.createElement('div');
     const petName = pet.pet_name ? pet.pet_name : 'Not Published';
@@ -89,18 +92,68 @@ const displayAllPets = pets => {
                 Price: ${petPrice}
                 </p>
                 <div class="card-actions flex justify-between">
-                  <button class="btn add-button">
+                  <button class="btn btn-sm add-button">
                     <i class="fa-regular fa-thumbs-up text-lg"></i>
                   </button>
-                  <button class="btn text-[#0E7A81] text-lg">Adopt</button>
-                  <button class="btn text-[#0E7A81] text-lg">Details</button>
+                  <button class="btn btn-sm text-[#0E7A81] text-lg">Adopt</button>
+                  <button id="" onclick="my_modal_1.showModal()" class="details-button btn text-[#0E7A81] btn-sm text-lg">Details</button>
                 </div>
               </div>
             </div>
 
   `;
     displayPets.appendChild(div);
+
+    // Add event listeners for each details button
+    const detailsButtons = document.querySelectorAll('.details-button');
+
+    detailsButtons.forEach((button, index) => {
+      button.addEventListener('click', () => {
+        const pet = pets[index];
+        const modalContainer = document.getElementById('modal-container');
+        modalContainer.innerHTML = `
+        <dialog id="my_modal_1" class="modal">
+          <div class="modal-box">
+          <img class="w-full rounded-lg" src=${pet.image}/>
+            <h2 class="font-extrabold text-xl">${
+              pet.pet_name || 'Not Available'
+            }</h2>
+            <div class="grid grid-cols-2"> 
+                <p>
+                  <i class="fa-solid fa-bowl-food"></i>
+                  ${pet.breed || 'Not Available'}
+                </p>
+                <p><i class="fa-regular fa-calendar-days "></i>
+                ${pet.date_of_birth || 'Not Available'}</p>
+                <p><i class="fa-solid fa-mercury"></i>
+                  Gender: ${pet.gender || 'Not Mentioned'}
+                </p>
+                <p><i class="fa-solid fa-dollar-sign"></i>
+                Price: ${pet.price || 'Not Available'}
+                </p>
+                <p><i class="fa-solid fa-mercury"></i>
+                ${pet.vaccinated_status || 'Not Available'}
+                </p>
+                </div>
+                <p class="mt-8 text-sm">
+                <span class="font-extrabold ">Details Information</span> <br>
+                ${pet.pet_details}</p>
+            <div class="modal-action ">
+            <form method="dialog" class="w-full">
+            <button class="btn bg-[#0E7A81] text-white hover:bg-[#0E7A86] text-lg font-bold w-full">Cancle</button>
+            </form>
+          </div>
+        </div>
+    </dialog>
+        `;
+        const modal = document.getElementById('my_modal_1');
+        if (modal) {
+          modal.showModal();
+        }
+      });
+    });
   });
+
   const addButton = document.querySelectorAll('.add-button');
   addButton.forEach((addButton, index) => {
     addButton.addEventListener('click', () => handleAddPet(pets[index]));
